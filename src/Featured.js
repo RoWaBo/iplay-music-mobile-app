a17241b1b810212b15d35694577e463ae4b10f78
Robert <u1501851@rts-365.dk>
2021-08-23 19:04:51 +0200

    Featured: fetched playlist from spotify as a test

---
+++ src/Featured.js
@@ -0,0 +1,36 @@
+import axios from "axios";
+import { useEffect, useState, useContext } from "react";
+import { TokenContext } from "./contexts/TokenContextProvider";
+
+const Featured = () => {
+
+    const [playlists, setPlaylists] = useState();
+    const { token } = useContext(TokenContext);
+
+    useEffect(() => {
+        if (token) {
+            axios("https://api.spotify.com/v1/browse/featured-playlists", {
+                headers: {
+                    "Authorization": token 
+                }
+            })
+            .then(result => setPlaylists(result.data.playlists.items))            
+        }
+    }, [token]);
+
+    playlists && console.log(playlists);
+
+    return (
+        <> 
+        <h1>Featured</h1>
+        {playlists && playlists.map(list => (
+            <div key={list.id}>
+                <h1>{list.name}</h1>
+                <img src={list.images[0].url} alt={list.name} />
+            </div>    
+        ))}
+        </>
+     );
+}
+ 
+export default Featured;
