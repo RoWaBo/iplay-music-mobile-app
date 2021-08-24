4076bd131b9eb7590b73d88483b7af30bdd3430c
Robert <u1501851@rts-365.dk>
2021-08-24 15:26:32 +0200

    Create useEffect to save authToken in sessionStorage

--- src/contexts/AuthContext.js
+++ src/contexts/AuthContext.js
@@ -1,16 +1,23 @@
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from 'react';
 
 export const AuthContext = createContext();
 
 const AuthContextProvider = ({ children }) => {
+  const [authToken, setAuthToken] = useState();
 
-    const [authToken, setAuthToken] = useState();
+  useEffect(() => {
+    if (authToken) {
+      sessionStorage.setItem('authToken', JSON.stringify(authToken));
+    } else {
+      setAuthToken(JSON.parse(sessionStorage.getItem('authToken')));
+    }
+  }, [authToken]);
 
-    return ( 
-        <AuthContext.Provider value={ {authToken, setAuthToken} }>
-            {children}
-        </AuthContext.Provider>
-     );
-}
- 
+  return (
+    <AuthContext.Provider value={{ authToken, setAuthToken }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
 export default AuthContextProvider;
