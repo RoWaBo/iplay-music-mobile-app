9177465cd71c634b8c3fef9a3d6912461fb5c8a6
Robert <u1501851@rts-365.dk>
2021-08-24 11:04:24 +0200

    Imported AuthContextProvider to App

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
-import { Router } from "@reach/router";
-import TokenContextProvider from "./contexts/TokenContextProvider";
-import Featured from "./views/Featured";
-import Login from "./views/Login";
+import { Router } from '@reach/router';
+import Featured from './views/Featured';
+import Login from './views/Login';
+import AuthContextProvider from './contexts/AuthContext';
+import TokenContextProvider from './contexts/TokenContext';
 
 function App() {
   return (
     <>
-      <TokenContextProvider>
-        <Router>
-          <Featured path="featured"/>
-          <Login path="login" />
-        </Router>
-      </TokenContextProvider>
+      <AuthContextProvider>
+        <TokenContextProvider>
+          <Router>
+            <Featured path="featured" />
+            <Login path="login" />
+          </Router>
+        </TokenContextProvider>
+      </AuthContextProvider>
     </>
   );
 }
