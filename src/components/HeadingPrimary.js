f428850c2ef3ec897dd1211cf336eb04990ce9a0
Robert <robertwattboolsen@gmail.com>
2021-08-26 15:52:21 +0200

    Created HeadingPrimary and added to featured

---
+++ src/components/HeadingPrimary.js
@@ -0,0 +1,17 @@
+/** @jsxImportSource @emotion/react */
+import { css } from '@emotion/react';
+import { font, spacing } from '../style/Styles';
+
+const HeadingPrimary = ({ children }) => {
+    return ( 
+        <h1 css={({colors}) => css`
+            color: ${colors.font.primary};
+            font-size: ${font.size.xl};
+            text-transform: capitalize;
+            margin: ${spacing.m}   
+        `}
+        >{ children }</h1>
+     );
+}
+ 
+export default HeadingPrimary;
--- src/views/Featured.js
+++ src/views/Featured.js
@@ -2,9 +2,10 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 import { useAuth } from "../contexts/AuthContext";
 /** @jsxImportSource @emotion/react */
-import { css } from '@emotion/react';
+import { css } from "@emotion/react";
 import NavigationBar from "../components/NavigationBar";
 import { navigate } from "@reach/router";
+import HeadingPrimary from "../components/HeadingPrimary";
 
 const Featured = () => {
 
@@ -12,7 +13,6 @@ const Featured = () => {
     const { authToken } = useAuth();
 
     useEffect(() => {
-        console.log(authToken);
         if (authToken) {
             axios("https://api.spotify.com/v1/browse/featured-playlists", {
                 headers: {
@@ -27,19 +27,16 @@ const Featured = () => {
         }
     }, [authToken]);
 
-    playlists && console.log(playlists);
-
     return (
-        <> 
-        <h1 css={theme => ({ color: theme.primary })}>Featured</h1>
+        <main css={({colors}) => css`background: ${colors.background.primary};`}> 
+        <HeadingPrimary>featured</HeadingPrimary>
         {playlists && playlists.map(list => (
             <div key={list.id}>
-                <h1>{list.name}</h1>
                 <img src={list.images[0].url} alt={list.name} />
             </div>    
         ))}
         <NavigationBar />
-        </>
+        </main>
      );
 }
  
