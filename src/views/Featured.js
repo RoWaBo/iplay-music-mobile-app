f6ce0b5969601d2fc3e82b02dc17f68551040c4e
Robert <robertwattboolsen@gmail.com>
2021-08-27 18:31:47 +0200

    Updated Featured to use SpotifyApiFetch function

--- src/views/Featured.js
+++ src/views/Featured.js
@@ -1,33 +1,14 @@
-import axios from "axios";
-import { useEffect, useState } from "react";
-import { useAuth } from "../contexts/AuthContext";
 /** @jsxImportSource @emotion/react */
 import { css } from "@emotion/react";
 import NavigationBar from "../components/NavigationBar";
-import { navigate } from "@reach/router";
 import HeadingPrimary from "../components/HeadingPrimary";
 import ShadowBox from "../components/ShadowBox";
 import { spacing } from "../style/Styles";
+import SpotifyApiFetch from "../components/SpotifyApiFetch";
 
 const Featured = () => {
 
-    const [playlists, setPlaylists] = useState();
-    const { authToken } = useAuth();
-
-    useEffect(() => {
-        if (authToken) {
-            axios("https://api.spotify.com/v1/browse/featured-playlists", {
-                headers: {
-                    "Authorization": `${authToken.token_type} ${authToken.access_token}`
-                }
-            })
-                .then(result => setPlaylists(result.data.playlists.items))
-                .catch(error => {
-                    navigate("/")
-                    // axios("https://accounts.spotify.com/api/token")           
-                })
-        }
-    }, [authToken]);
+    const playlists = SpotifyApiFetch("https://api.spotify.com/v1/browse/featured-playlists")   
 
     const contentContainer = css`
         margin: ${spacing.m};
@@ -44,7 +25,7 @@ const Featured = () => {
         <main css={({ colors }) => css`background: ${colors.background.primary};`}>
             <HeadingPrimary />
             <div css={contentContainer}>
-                {playlists && playlists.map(list => (
+                {playlists && playlists.data.playlists.items.map(list => (
                     <ShadowBox key={list.id}>
                         <img src={list.images[0].url} alt={list.name} />
                     </ShadowBox>
