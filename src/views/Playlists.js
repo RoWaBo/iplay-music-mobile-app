bba2437341264c5f34577c08279e0469d4c69bdd
Robert <robertwattboolsen@gmail.com>
2021-08-28 11:42:59 +0200

    Testing playing sound from spotify url

--- src/views/Playlists.js
+++ src/views/Playlists.js
@@ -6,11 +6,16 @@ import SpotifyApiFetch from '../components/SpotifyApiFetch';
 
 const Playlists = ({ playlistId }) => {
 
-    SpotifyApiFetch(`https://api.spotify.com/v1/playlists/${playlistId}`) 
+    const playlist = playlistId !== "featured" && SpotifyApiFetch(`https://api.spotify.com/v1/playlists/${playlistId}`) 
+
+    const track = playlist && playlist.data.tracks.items[0].track.preview_url
+
+    const audio = new Audio(track)
 
     return ( 
         <main css={({colors}) => css`background: ${colors.background.primary};`}> 
         <HeadingPrimary />
+        <button onClick={() => audio.play()} >play</button>
         <NavigationBar />
         </main>
      );
