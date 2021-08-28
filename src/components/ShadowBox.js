dfb9c49e1032bfba8a4792b035bb0153607b1da9
Robert <robertwattboolsen@gmail.com>
2021-08-28 13:20:09 +0200

    Added min-with and height to ShadowBox

--- src/components/ShadowBox.js
+++ src/components/ShadowBox.js
@@ -3,9 +3,9 @@ import { css } from "@emotion/react";
 
 const ShadowBox = ({ children }) => {
 
-    const styling = ({colors}) => css`
-        width: 100%;
-        height: 100%;
+    const styling = css`
+        min-width: 130px;
+        min-height: 130px;
         border-radius: 8px;
         box-shadow: 0px 0px 25px #00000026;
         overflow: hidden;
