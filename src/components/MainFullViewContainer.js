5ed6b3a63975e15148a32605a07d2680403d361a
Robert <robertwattboolsen@gmail.com>
2021-08-30 09:05:13 +0200

    Created MainFullViewContainer component

--- src/components/MainFullViewContainer.js
+++ src/components/MainFullViewContainer.js
@@ -5,6 +5,7 @@ const MainFullViewContainer = ({ children }) => {
 
     const style = ({ colors }) => css`
             background: ${colors.background.primary};
+            padding-bottom: 52.8px; 
         `    
 
     return <main css={style}>{ children }</main>
