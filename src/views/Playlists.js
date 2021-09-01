62db5d3b0d73162e30059ea196ab7761e6371b4c
Robert <robertwattboolsen@gmail.com>
2021-09-01 08:16:02 +0200

    Added svg gradient background to Playlists

--- src/views/Playlists.js
+++ src/views/Playlists.js
@@ -3,17 +3,30 @@ import { css } from '@emotion/react';
 import NavigationBar from '../components/NavigationBar';
 import HeadingPrimary from '../components/HeadingPrimary';
 import useSpotifyApiFetch from '../functions/useSpotifyApiFetch';
+import MainFullViewContainer from "../components/MainFullViewContainer";
+import UtilityBar from '../components/UtilityBar';
 
 const Playlists = ({ playlistId }) => {
 
     // const playlist = playlistId !== "featured" && useSpotifyApiFetch(`https://api.spotify.com/v1/playlists/${playlistId}`) 
 
-    return ( 
-        <main css={({colors}) => css`background: ${colors.background.primary};`}> 
-        <HeadingPrimary />
-        <NavigationBar />
-        </main>
-     );
+    const backgroundImg = ({ colors }) => css`
+        background-image: url('/sound-wave.svg');
+        max-width: 100%;
+        height: 275px;
+        background-size: cover;
+        background-position: center;
+    `
+
+    return (
+        <MainFullViewContainer>
+            <UtilityBar light heading="playlists" />
+            <section css={backgroundImg}>
+                <HeadingPrimary light>playlists</HeadingPrimary>
+            </section>
+            <NavigationBar />
+        </MainFullViewContainer>
+    );
 }
- 
+
 export default Playlists;
