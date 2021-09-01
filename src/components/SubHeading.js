f7ff7ebd2cf7e63b2cb683cae7d1afc6cb1ef7c1
Robert <robertwattboolsen@gmail.com>
2021-09-01 10:34:14 +0200

    Created new prop for SubHeading

--- src/components/SubHeading.js
+++ src/components/SubHeading.js
@@ -2,11 +2,12 @@
 import { css } from '@emotion/react';
 import { font } from '../style/Styles';
 
-const SubHeading = ({ children }) => {
+const SubHeading = ({ children, large }) => {
 
     const heading = ({ colors }) => css`
         color: ${colors.font.primary};
-        font-size: ${font.size.m};
+        font-size: ${large ? font.size.l : font.size.m};
+        text-align: ${large ? 'center' : 'unset'}; 
         font-weight: ${font.weight.bold};
         text-transform: capitalize;
     `
