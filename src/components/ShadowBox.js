dbad38ba8bcb2e1bcd059ab683844de15736117b
Robert <robertwattboolsen@gmail.com>
2021-09-02 15:45:31 +0200

    Added xSmall ShadowBox variant

--- src/components/ShadowBox.js
+++ src/components/ShadowBox.js
@@ -1,7 +1,7 @@
 /** @jsxImportSource @emotion/react */
 import { css } from "@emotion/react";
 
-const ShadowBox = ({ children, small, medium }) => {
+const ShadowBox = ({ children, xSmall, small, medium, circle }) => {
 
     const defaultSize = `min-width: 130px; min-height: 130px;`        
     const mediumSize = `
@@ -16,12 +16,19 @@ const ShadowBox = ({ children, small, medium }) => {
         max-width: 50px; 
         max-height: 50px;
     `        
+    const xSmallSize = `
+        min-width: 40px; 
+        min-height: 40px;
+        max-width: 40px; 
+        max-height: 40px;
+    `        
     
     const styling = css`
         ${small && smallSize};
         ${medium && mediumSize};
-        ${!small && !medium && defaultSize};
-        border-radius: 8px;
+        ${xSmall && xSmallSize};
+        ${!small && !medium && !xSmall && defaultSize};
+        border-radius: ${circle ? "50%" : "8px"};
         box-shadow: 0px 0px 25px #00000026;
         overflow: hidden;
         object-fit: cover;
