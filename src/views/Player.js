3076ae3f80956ae7089ba426ec640b8c0d358463
Robert <robertwattboolsen@gmail.com>
2021-09-03 08:43:57 +0200

    Created light and dark condition on background image on Player

--- src/views/Player.js
+++ src/views/Player.js
@@ -1,14 +1,28 @@
 /** @jsxImportSource @emotion/react */
 import { css } from '@emotion/react';
-import NavigationBar from '../components/NavigationBar';
-import HeadingPrimary from '../components/HeadingPrimary';
+import MainFullViewContainer from '../components/MainFullViewContainer';
+import UtilityBar from '../components/UtilityBar';
+import { useTheme } from "../contexts/ThemeContext";
 
 const Player = () => {
+
+    const { theme } = useTheme()
+    
+    const imgContainer = css`
+        background-image: url('vinyl.png'), ${ theme === 'light' ? `url('sound-wave-light.png')` : `url('sound-wave-dark.png')`};
+        background-position: center;
+        background-repeat: no-repeat;
+        height: 400px;
+        margin-top: 81px;
+    `
+
     return ( 
-        <main css={({colors}) => css`background: ${colors.background.primary};`}> 
-        <HeadingPrimary />
-        <NavigationBar />
-        </main>
+        <MainFullViewContainer>
+            <UtilityBar heading="playing"/>
+            <div css={imgContainer}>
+
+            </div>
+        </MainFullViewContainer>
      );
 }
  
