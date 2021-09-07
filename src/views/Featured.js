0c2db661edfa3bae25cda83139a834212b5dbf0c
Robert <robertwattboolsen@gmail.com>
2021-09-07 09:44:27 +0200

    Added lazyImgObserver to helperfunctions and imported to Featured

--- src/views/Featured.js
+++ src/views/Featured.js
@@ -9,38 +9,12 @@ import { Link } from "@reach/router";
 import UtilityBar from "../components/UtilityBar";
 import MainFullViewContainer from "../components/MainFullViewContainer";
 import { useEffect, useRef } from "react";
+import { lazyImgObserver } from "../functions/HelperFunctions";
 
 const Featured = () => {
 
     const playlists = useSpotifyApiFetch("https://api.spotify.com/v1/browse/featured-playlists")
     const lazyLoadeParent = useRef()
-
-    // Options for IntersectionObserver
-    const optionsObject = {
-        // loading the img/calling the callbackfunction -200px before img is in viewport
-        rootMargin: "0px 0px -200px",
-        // THRESHOLD controls when we call the callback function
-        // 1 means 100% of the picture is visible
-        // Here we have 3 callbacks one at 0%, 50%, 100% img visiblility 
-        threshold: [0, 0.5, 1]
-    }
-
-    // IntersectionObserver takes 2 parameters: a function and an options object
-    const lazyImgObserver = new IntersectionObserver(entries => {
-        // In this case entries is all observed images
-        entries.forEach(entry => {
-            const img = entry.target.querySelector('img')
-            // isIntersecting is true if the observed img is in the viewport
-            if (entry.isIntersecting) {
-                // replaces the placeholder svg with the main img 
-                img.src = img.dataset.src
-                // removes the attribute which stored the main img url 
-                img.removeAttribute("data-src");
-                // Stops observing the img 
-                lazyImgObserver.unobserve(entry.target);
-            }
-        })
-    }, optionsObject);
 
     // Adds IntersectionObserver to all images
     useEffect(() => {
@@ -59,7 +33,6 @@ const Featured = () => {
             margin-bottom: ${spacing.xl};    
         }
     `
-
     return (
         <MainFullViewContainer>
             <UtilityBar heading="Featured" />
